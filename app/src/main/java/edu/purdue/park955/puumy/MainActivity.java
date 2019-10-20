be2bc91ef38a0e430050b5c4ad92c319f1bfec11
Jeonghu <park955@purdue.edu>
2019-10-20 15:55:08 -0400

    Initial Commit

---
+++ app/src/main/java/edu/purdue/park955/puumy/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.purdue.park955.puumy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
