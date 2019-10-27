1cdeb3f6aaf26f449f0240e1fec7262856c313ba
kim3089 <kim3089@purdue.edu>
2019-10-27 16:31:25 -0400

    Jisoo initial attempts

--- app/src/main/java/edu/purdue/park955/puumy/MainActivity.java
+++ app/src/main/java/edu/purdue/park955/puumy/MainActivity.java
@@ -1,81 +1,49 @@
 package edu.purdue.park955.puumy;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Declaring Views as fields so we can use it later
-    TextView introText;
-    TextView statusText;
-    Button sampleButton;
-    Button saveButton;
     SeekBar seekBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Declaring the fields onCreate so we can use it later
-        this.getViews();
+        Button btn = (Button) findViewById(R.id.done);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MyApp", "This is a magic log message!");
+                Toast.makeText(getApplicationContext(), "Mood recorded!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
 
-        //How to load a saved Data
-        int loadValue = this.loadData("SeekBarValue");
+        seekBar=(SeekBar)findViewById(R.id.moodBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                Toast.makeText(getApplicationContext(),"seekbar progress: "+progress, Toast.LENGTH_SHORT).show();
+            }
 
-        //Setting some initial values
-        saveButton.setText("Save");
-        if(loadValue == -1) {
-            statusText.setText("No values to load");
-        } else {
-            statusText.setText("Loaded Value: "+loadValue);
-        }
-    }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"seekbar touch started!", Toast.LENGTH_SHORT).show();
+            }
 
-    //How to get View components into backend Java code
-    private void getViews(){
-        this.introText = findViewById(R.id.introText);
-        this.statusText = findViewById(R.id.statusText);
-        this.sampleButton = findViewById(R.id.sampleButton);
-        this.saveButton = findViewById(R.id.saveButton);
-        this.seekBar = findViewById(R.id.seekBar);
-    }
-
-    //How to create a simple Button function
-    public void pressedSampleButton(View view){
-        Context context = getApplicationContext();
-        Toast.makeText(context, "Hi, there! I'm Pummy!", Toast.LENGTH_SHORT).show();
-    }
-
-    //How to save a data
-    public void pressedSaveButton(View view){
-
-        //Get values from the seekBar
-        int value = this.seekBar.getProgress();
-
-        //Saving the value using sharedPreference
-        SharedPreferences sp = this.getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
-        sp.edit().putInt("SeekBarValue", value).apply();
-
-        //Toast to show the user that it is saved
-        Context context = getApplicationContext();
-        Toast.makeText(context, "The value "+value+" is saved",Toast.LENGTH_SHORT).show();
-    }
-
-    //How to load a data
-    public int loadData(String key){
-
-        //Loading the value using sharedPreference
-        SharedPreferences sp = this.getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
-        return sp.getInt(key,-1);
-
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"seekbar touch stopped!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
